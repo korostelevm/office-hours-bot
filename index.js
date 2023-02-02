49d2fd03686a1c650714dbb01976dbe0dce2864e
Mike <korostelevm@gmail.com>
2023-02-02 13:34:40 -0500

    asdf

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+process.env.PUBLIC_KEY = 'a9314b598d3fd1de1f1850fc3ac369e6bcd6d138b1c7f40e64dc0c991411f08b'
+
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+
+const { verifyKeyMiddleware } = require('discord-interactions')
+
+app.post('/', verifyKeyMiddleware(process.env.PUBLIC_KEY), (req, res) => {
+    const message = req.body;
+    console.log(message)
+    // if (message.type === InteractionType.APPLICATION_COMMAND) {
+    //   res.send({
+    //     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    //     data: {
+    //       content: 'Hello world',
+    //     },
+    //   });
+    // }
+    return res.send('ok')
+  });
 app.listen(process.env.PORT || 3000)
